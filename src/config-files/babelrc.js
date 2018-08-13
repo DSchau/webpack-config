65966135d7e7f8186e5c417d4805d7ac981863e8
Dustin Schau <dustinschau@gmail.com>
2018-08-13 17:20:04 -0400

    chore: prepare for deploy

--- src/config-files/babelrc.js
+++ src/config-files/babelrc.js
@@ -3,14 +3,14 @@ const env = process.env.NODE_ENV;
 module.exports = {
   presets: [
     [
-      'env',
+      "env",
       {
         targets: {
-          browsers: ['last 2 versions', 'safari >= 7']
+          browsers: [">0.25%", "not ie 11", "not op_mini all"]
         },
-        modules: env === 'test' ? 'commonjs' : false
+        modules: env === "test" ? "commonjs" : false
       }
     ]
   ],
-  plugins: ['syntax-dynamic-import']
+  plugins: ["syntax-dynamic-import"]
 };
